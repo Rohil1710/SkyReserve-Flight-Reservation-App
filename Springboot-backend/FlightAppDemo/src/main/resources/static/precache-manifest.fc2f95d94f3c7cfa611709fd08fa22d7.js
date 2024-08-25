d5284adc2bfac2d2fc9e8506f575c7bb48ce9589
Rohil1710 <rohil1710@gmail.com>
2024-08-25 06:42:04 -0600

    Adding the springboot backend

---
+++ Springboot-backend/FlightAppDemo/src/main/resources/static/precache-manifest.fc2f95d94f3c7cfa611709fd08fa22d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599d42cf486ff389985de11b13524550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e868bfb060037848997a",
+    "url": "/static/css/main.4bfe601b.chunk.css"
+  },
+  {
+    "revision": "d673495d1396e46bb2aa",
+    "url": "/static/js/2.1d854cd9.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "e868bfb060037848997a",
+    "url": "/static/js/main.051442b0.chunk.js"
+  },
+  {
+    "revision": "02558d794f1b309512ce",
+    "url": "/static/js/runtime~main.f27e42ef.js"
+  }
+]);
